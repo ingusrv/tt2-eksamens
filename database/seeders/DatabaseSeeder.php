4c0d4ecab7867d601f8d5e84e3ba76e6a3032782
ingusrv <ingus.valheims@gmail.com>
2024-06-25 03:37:40 +0300

    database schema un nedaudz seeder dati

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Board;
+use App\Models\Column;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,45 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory()->create(
+            [
+                'name' => 'parasts lietotājs',
+                'email' => 'normal@kanban.test',
+                "role" => 0
+            ]
+        );
+
+        $normalUser = User::query()->latest()->first();
+
+        Board::create([
+            "name" => "first board",
+            "user_id" => $normalUser->id,
+            "privacy" => 0
         ]);
+
+        $firstBoard = Board::query()->latest()->first();
+
+        Column::create([
+            "name" => "first column",
+            "board_id" => $firstBoard->id,
+            "order" => 0
+        ]);
+
+        $firstColumn = Column::query()->latest()->first();
+
+        $firstColumn->cards()->createMany([
+            ["text" => fake()->sentence(), "order" => 0],
+            ["text" => fake()->sentence(), "order" => 1],
+            ["text" => fake()->sentence(), "order" => 2],
+            ["text" => fake()->sentence(), "order" => 3],
+        ]);
+
+        User::factory()->create(
+            [
+                "name" => "administrators",
+                "email" => "admin@kanban.test",
+                "role" => 1
+            ]
+        );
     }
 }
